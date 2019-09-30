7636016a244dd5704230cdd65f37062400be6a17
MR_Ninja <54943600+KuntuL397@users.noreply.github.com>
2019-09-30 09:59:43 +0800

    Add files via upload

---
+++ ninja.sh
@@ -0,0 +1,203 @@
+#!/bin/bash
+clear
+r='\033[31;2m' #merah
+g='\003[32;1m' #hijau]
+b='\033[34;2m' #biru
+p='\033[35;1m' #purple
+cy='\033[36;2m' #cyan
+w='\033[37;1m' #putih
+y='\033[33;1m' #kuning
+
+# [coded by] MR_Ninja
+
+echo $r""
+figlet -f standard MR.NINJA
+echo
+sleep 1
+echo $p"["$p"===================================="$p"]"
+echo $p"["$r"=>"$p"]"$r" Author:MR.NINJA
+echo $p"["$r"=>"$p"]"$r" Contact:081999265308
+echo $p"["$r"=>"$p"]"$r" MASTER OF DESTRUCTION
+echo $p"["$p"===================================="$p"]"
+echo
+sleep 2
+echo $p"["$cy"01"$p"]"$w" Hiburan Cok_-"
+sleep 1
+echo $p"["$cy"02"$p"]"$" Ping Checker"
+sleep 1
+echo $p"["$cy"03"$p"]"$w" Hack Facebook"
+sleep 1
+echo $p"["$cy"04"$p"]"$w" Tools Installer"
+sleep 1
+echo $p"["$cy"05"$p"]"$w" Tools DDOS"
+sleep 1
+echo $p"["$cy"06"$p"]"$w" Hack Whatsapp"
+sleep 1
+echo $p"["$cy"07"$p"]"$w" Percepat Signal"
+sleep 1
+echo $p"["$cy"∆"$p"]"$g" Tunggu Update Selanjutnya Ok"
+sleep 1
+echo $p"║"
+read -p"╚═[pilih]=>" Mt
+
+if [ $mt = 1 ]
+then
+clear
+echo $p""
+figlet -f slant "WAIT.."
+sleep 1
+echo $p"["$w"+"$p"]" $w"mencari musik"
+sleep 1
+echo $p"["$w"+"$p"]" $w"musik ini berjudul kekerasan:v"
+sleep 1
+echo $p"["$w"+"$p"]" $w"......."
+sleep 1
+clear
+sleep 1
+echo $p"termux music"
+echo $w"Lirik Lagu Asede Kontol:v"
+echo
+sleep 1
+echo $p"["$w"+"$p"]" $w" asede kontol"
+echo
+sleep 1
+echo $p"["$w"+"$p"]" $w" sama lo semua"
+echo
+sleep 1
+echo $p"["$w"+"$p"]" $w" ngentot ngentot"
+echo
+sleep 1
+echo $p"["$w"+"$p"]" $w" sama lo semua
+echo
+sleep 1
+echo$p"Thankyou"$w" kami sekecamatanan mengucapkan"
+echo $p"BODOAMAT:v"
+sleep 1
+echo $w"dasar gabut_-"
+sleep 3
+sh ninja.sh
+fi
+
+if [ $mt = 2]
+then
+clear
+echo $p""
+figlet -f standard MR.NINJA
+echo
+echo $r"["$p"=================================$r"]"
+echo $r"["$p"+"$r"]"$p" Di Buat Khusus Kartu Indo"
+echo $r"["$p"+"$r"]"$p" dilarang pake suu_-"
+echo $r"["$p"+"$r"]"$p" kecuali Kartu Indo"
+echo $r"["$p"=================================$r"]"
+echo
+sleep 2
+clear
+echo $r""
+figlet -f slant WAIT.."
+sleep 2
+echo $r"["$w"+"$r"]" $p"sedang mencari server yng bagus"
+sleep 2
+echo $r"["$w"+"$r"]" $p"menuju server yng bagus"
+echo $r"["$w"+"$r"]" $p"connection...."
+sleep 1
+ping google.com
+fi
+
+if [ $mt = 3 ]
+then
+clear
+echo $p""
+figlet -f standard MR.NINJA
+echo
+echo $p"["$w"==========================================="$p"]"
+echo $p"["$w"+"$p"]"$w" Author : mhank Tutor"
+echo $p"["$w"+"$p"]"$w" Youtube : Mhank Tuttorr"
+echo $p"["$w"+"$p"]"$w" Script installer Dark-Fb v1.8"
+echo $p"["$w"==========================================="$p"]"
+echo
+sleep 2
+echo $p"["$w"1"$p"]"$w" memasang..."$p"Facebook"
+echo $r""
+figlet -f slant "WAIT.."
+sleep 2
+pkg install python2;pip2 install bs4;pip2 install requests;pip2 install mechanize;pkg install openssh;pkg install php;python2 fb.py
+fi
+
+if [ $mt = 4 ]
+then
+clear
+echo $p""
+figlet -f standard MR.NINJA
+echo
+echo $p"["$w"======================================="$p"]"
+echo $p"["$w"+"$p"]"$w" Script installer Tn B4DUT"
+echo $p"["$w"+"$p"]"$w" Hekel pasti ini"
+echo $p"["$w"+"$p"]"$w" Dilarang Menggunakanya"
+echo $p"["$w"======================================="$p"]"
+echo
+sleep 2
+echo $p"["$w"1"$p"]"$w" Memasang...."
+echo $r""
+figlet -f slant "WAIT.."
+sleep 2
+apt update;apt upgrade -y;apt install git;apt install php;apt install curl;apt install ruby;apt install figlet;apt install python2;gem install lolcat;git clone https://github.com/TUANB4DUT/TOOLSINSTALLERv3;cd TOOLSINSTALLERv3;chmod +x TUANB4DUT.sh;sh TUANB4DUT.sh
+fi
+
+if [ $mt = 5 ]
+then
+clear
+echo $p""
+figlet -f standard MR.NINJA
+echo 
+echo $p"<"$r"===]==========================================>"
+echo $p"["$r"+"$w" Tools DDOS Hammer
+echo $p"<"$r"==========================================[===>"
+echo
+echo $p"=>"$p"Cara Penggunaan: python3 hammer.py -s (IP adrees) -p 80 -t 135 cara mencari alamat ip web: ping (nama website) lalu enter"
+sleep 6
+clear
+figlet -f slant "WAIT.."
+sleep 1
+pkg update && pkg upgrade;pkg install python;pkg install git;git clone https://github.com/cyweb/hammer;cd hammer;ls;python hammer.py
+fi
+
+if [ $mt = 6 ]
+then
+clear
+echo $p"["$r"+"$p" Hack Whatsapp"
+sleep 1
+sh wa.sh
+fi
+
+if [ $mt = 7 ]
+then
+clear
+echo $p""
+figlet -f standard MR.NINJA
+echo
+echo $p"["$w"======================================="p"]"
+echo $p"["$w"+"$p"]"$w" Sinyal Stabilizer"
+echo $p"["$w"+"$p"]"$w" Dibuat Khusus Indonesia"
+echo $p"["$w"+"$p"]"$w" Dilarang Memakai Tools Ini"
+echo $p"["$w"+"$p"]"$w" Kecuali Orng "$p"indo:v"
+echo $p"["$w"======================================"$p"]"
+echo
+sleep 2
+echo $p"["$w"1"$p"]"$w" Menstabilkan.."
+echo $r""
+figlet -f slant "WAIT.."
+sleep 2
+ping 8.8.8.8
+fi
+
+if [ $mt = ∆ ]
+then 
+clear
+echo $p""
+figlet -f standard MR.NINJA
+echo
+sleep 1
+echo $r "coming soon ya:v"
+sleep 4
+sh ninja.sh
+fi
